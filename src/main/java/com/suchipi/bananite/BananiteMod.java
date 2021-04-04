859bc7209a5b5094b24002373028e3c6f059cf55
Suchipi <me@suchipi.com>
2021-04-04 02:26:51 -0600

    Add banana item

--- src/main/java/com/suchipi/bananite/BananiteMod.java
+++ src/main/java/com/suchipi/bananite/BananiteMod.java
@@ -2,6 +2,7 @@ package com.suchipi.bananite;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,13 +20,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(BananiteMod.MODID)
+public class BananiteMod
 {
+    public static final String MODID = "bananite";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public BananiteMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -37,6 +40,8 @@ public class ExampleMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        RegistryHandler.init();
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -48,7 +53,7 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
---
+++ src/main/java/com/suchipi/bananite/RegistryHandler.java
@@ -0,0 +1,27 @@
+package com.suchipi.bananite;
+
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    // create DeferredRegister object
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BananiteMod.MODID);
+
+    public static void init() {
+        // attach DeferredRegister to the event bus
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // register item
+    public static final RegistryObject<Item> BANANA = ITEMS.register("banana", () ->
+            new Item(new Item.Properties().group(ItemGroup.FOOD).food(
+                    new Food.Builder().saturation(3).hunger(3).build()
+            ))
+
+    );
+}
